76df4616b2d41ff3a2acf253ce1baea3006acf24
andrewstech <info@andrewstech.me>
2021-03-19 18:44:05 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='youtube-alexa-python',
+    version='1.2.3',
+    packages=[''],
+    url='https://github.com/unofficial-skills/youtube-alexa-python',
+    license='Apache License, Version 2.0',
+    author='ADMIN',
+    author_email='hello@andrewstech.me',
+    description='YouTube for Alexa/Echo device'
+)
