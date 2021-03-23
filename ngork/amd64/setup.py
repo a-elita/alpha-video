f07394a273be77017d48ec311503e2cb8cf60ccd
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-03-23 17:19:47 +0000

    Added Ngork package

---
+++ ngork/amd64/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask-ngrok",
+    version="0.0.26",
+    author="Grant Stafford",
+    description="A simple way to demo Flask apps from your machine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gstaff/flask-ngrok",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='flask ngrok demo',
+    install_requires=['Flask>=0.8', 'requests'],
+    py_modules=['flask_ngrok']
+)
