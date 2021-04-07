aeabc5bd68e2ab462cd47169a2b6897d20fe6f95
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-04-07 23:54:53 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    About: ['doc1', 'contributions'],
+    About: ['start', 'doc1', 'contributions'],
     Prerequisites: ['doc2'],
     setup: ['doc3', 'doc4', 'doc5'],
   },
